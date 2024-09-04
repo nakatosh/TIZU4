a032278a9b70c2afc81f12e84c96ab5f9af72a27
nakatosh <92030557+nakatosh@users.noreply.github.com>
2024-09-04 13:44:32 +0900

    Update t.js

--- t.js
+++ t.js
@@ -400,32 +400,52 @@ if (etc === '1' && st !== '999'){
 //管理NOリストBOXに格納　重複あり
 
 function KANRINOa(event) {
+
   return new Promise(function(resolve) {
     var result = document.getElementById("result");                   
     var transaction = db.transaction(["mystore"], "readwrite");
     var store = transaction.objectStore("mystore");
     var request = store.openCursor();
-
+    let array =[];
   
     request.onsuccess = function (event) {
-      //リストがなかったら終了  
+
+      //リストがなくなったら終了  
       if(event.target.result == null) {
-      resolve()   
+      resolve()
+	//重複削除
+	let res = new Set(array)  
+	
+	res.forEach((element)=>{
+    
+    	// option要素を生成
+    	let option = document.createElement('option');
+    
+    	option.text = element;
+   	 option.value = element;
+    
+   	 // 生成したoption要素をselect要素に追加
+  	  document.getElementById("PullDownList").add(option);
+		document.getElementById("PullDownList").value = "";
+	});
+
+
       return;
-      }
+    }
+
       var cursor = event.target.result;
       var data = cursor.value;
 
-      let GCD = document.createElement("option");
-      GCD.value = data.myKno;  //value値
-      GCD.text = data.myKno;   //テキスト値
-  	document.getElementById("PullDownList").appendChild(GCD);
+	array.unshift( data.myKno);
 	
-     cursor.continue();
-	document.getElementById("PullDownList").value = "";
+  cursor.continue();
+
+
     }
 
   })
+
+
 
 }
 
